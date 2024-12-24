ea62168887b1e62cf1d4bdb8399a36ba8ff8e193
jeongmin <mymemose@naver.com>
2024-12-24 15:01:19 +0900

    fix: appbar heght 수정

--- lib/main/screen/main_screen.dart
+++ lib/main/screen/main_screen.dart
@@ -11,11 +11,14 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Image.asset("assets/logo.png"),
-        actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.notifications))
-        ],
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(64),
+        child: AppBar(
+          title: Image.asset("assets/logo.png"),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.notifications)),
+          ],
+        ),
       ),
       body: Text("메인"),
     );
