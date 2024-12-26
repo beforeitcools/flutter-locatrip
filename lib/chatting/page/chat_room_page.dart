a7fb6fd1ef3394c4fa4852da1316676ef811ef4d
yzzzang <yz1211ch@gmail.com>
2024-12-26 15:56:51 +0900

    feat: 채팅방 레이아웃

---
+++ lib/chatting/page/chat_room_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_locatrip/chatting/model/chat_model.dart';
+import 'package:flutter_locatrip/chatting/page/chat_room_setting.dart';
+import 'package:flutter_locatrip/common/widget/color.dart';
+
+class ChatRoomPage extends StatefulWidget {
+  const ChatRoomPage({super.key, required this.chatModel});
+  final ChatModel chatModel;
+
+  @override
+  State<ChatRoomPage> createState() => _ChatRoomPageState();
+}
+
+class _ChatRoomPageState extends State<ChatRoomPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        leading: InkWell(
+          onTap: (){Navigator.pop(context);},
+          child:  Icon(Icons.arrow_back),
+        ),
+        title: Text(widget.chatModel.name),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.search), color: grayColor),
+          IconButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>ChatRoomSetting()));}, icon: Icon(Icons.settings_outlined), color: grayColor)
+        ],),
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Stack(
+            children: [
+              ListView(),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Row(
+                  children: [
+                    Padding(padding: EdgeInsets.only(left: 16, bottom: 16),
+                      child: IconButton(onPressed: (){}, icon: Icon(Icons.add_circle_outline), color: blackColor, iconSize: 30,),
+                    ),
+                    Expanded(child: Container(
+                      padding: EdgeInsets.only(right: 16, bottom: 16),
+                        width: MediaQuery.of(context).size.width - 55,
+                        child: Card(
+                            elevation: 0,
+                            color: Color.fromRGBO(170, 170, 170, 0.1),
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+                            child: Row(
+                              children: [
+                                Expanded(child: TextFormField(
+                                  style: Theme.of(context).textTheme.bodyMedium,
+                                  textAlignVertical: TextAlignVertical.center,
+                                  keyboardType: TextInputType.multiline,
+                                  maxLines: 10,
+                                  minLines: 1,
+                                  decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: "메세지를 입력하세요",
+                                      contentPadding: EdgeInsets.only(left: 20, right: 20, bottom: 15)),
+                                )),
+                                IconButton(onPressed: (){}, icon: Icon(Icons.send), color: grayColor,)
+                              ],
+                            )))),
+                ]),
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
---
+++ lib/chatting/page/chat_room_setting.dart
@@ -0,0 +1,57 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_locatrip/common/widget/color.dart';
+
+class ChatRoomSetting extends StatelessWidget {
+  const ChatRoomSetting({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: InkWell(
+            onTap: (){Navigator.pop(context);},
+            child:  Icon(Icons.close),
+          ),
+        title: Text("채팅방 설정", style: Theme.of(context).textTheme.bodyLarge),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                child: CircleAvatar(
+                    radius: 40,
+                    child: IconButton(
+                      icon: Icon(Icons.camera_alt, color: Colors.white),
+                      onPressed: () {},
+                    ))),
+              Container(
+                padding: EdgeInsets.only(top: 30, bottom: 60),
+                child: Row(
+                  children: [
+                    Text("채팅방 이름", style: Theme.of(context).textTheme.bodyMedium),
+                    Expanded(child: Container(
+                      child: Text("울릉도동남쪽뱃길따라87k외로운섬", style: Theme.of(context).textTheme.bodySmall, textAlign: TextAlign.right) //TODO 20자 넘어가지 못하게 할 것임
+                    ))
+                  ])),
+              SizedBox(
+                width: 300, height: 56,
+                child: FilledButton(
+                    onPressed: (){Navigator.pop(context); }, //TODO 채팅방 나가는 로직 추가
+                    child: Text("채팅방 나가기",
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: Colors.white)),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: pointBlueColor,
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)))
+                ),
+              )
+            ]))
+      )
+    );
+  }
+}
