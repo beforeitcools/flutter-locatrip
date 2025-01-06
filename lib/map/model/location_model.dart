e5f365522f7b0cef3c045d2073344851294c0fcc
NJ-KIM11 <alwaysred1116@gmail.com>
2025-01-06 18:01:20 +0900

    commit to merge

---
+++ lib/map/model/location_model.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_locatrip/map/model/place.dart';
+
+class LocationModel {
+  Future<dynamic> insertLocation(Map<String, dynamic> place) async {
+    final dio = Dio();
+
+    try {
+      // final responses = await dio.post("http://192.168.45.79:8082/trip/insert",
+      final responses = await dio
+          .post("http://112.221.66.174:1234/location/insert", data: place);
+      if (responses.statusCode == 200) {
+        return responses.data as Map<String, dynamic>;
+      } else {
+        return "장소 저장에 실패했습니다.";
+      }
+    } catch (e) {
+      print(e);
+      throw Exception("Error : $e");
+    }
+  }
+}
