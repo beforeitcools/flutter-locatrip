ac52f62ff3c9f5f1edb47fb50ee844978da45bbc
yzzzang <yz1211ch@gmail.com>
2025-01-08 18:17:28 +0900

    fix: link 연결 시 데이터 가져오는 로직 수정 중

--- lib/chatting/model/link_model.dart
+++ lib/chatting/model/link_model.dart
@@ -1,23 +1,26 @@
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_locatrip/common/Auth/auth_dio_interceptor.dart';
 import 'package:flutter_locatrip/common/widget/url.dart';
 
 class LinkModel{
   final linkRegex = RegExp(r'(https?:\/\/[^\s]+)');
 
-  void detectAndPreviewLink(String message){
+  void detectAndPreviewLink(String message, BuildContext context){
     final matches = linkRegex.allMatches(message);
     for(var match in matches){
       final link = match.group(0);
       if(link != null){
-        fetchLinkPreview(link); // 서버로 링크 정보 요청
+        fetchLinkPreview(link, context); // 서버로 링크 정보 요청
       }
     }
   }
 
-  Future<void> fetchLinkPreview(String link) async{
+  Future<void> fetchLinkPreview(String link, BuildContext context) async{
     final dio = Dio();
+    dio.interceptors.add(AuthInterceptor(dio, context));
 
     try{
       final response = await dio.post('$backUrl/link-preview');
