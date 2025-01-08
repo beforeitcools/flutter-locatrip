f47cfe455b47ea61cfb87c6a502343792d6e5f02
yzzzang <yz1211ch@gmail.com>
2025-01-08 17:23:50 +0900

    fix: 최신순 필터 적용, 디자인 수정

--- lib/advice/widget/post_filter.dart
+++ lib/advice/widget/post_filter.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_locatrip/common/widget/color.dart';
 
 class PostFilter extends StatefulWidget {
   const PostFilter({super.key});
@@ -23,19 +24,35 @@ class _PostFilterState extends State<PostFilter> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.all(16),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          DropdownButton(
-              value: _selectedFilter,
-              items: _filters.map((select)=>DropdownMenuItem(child: Text(select), value: select)).toList(),
-              onChanged: (value){
-                setState(() {
-                  _selectedFilter = value!;
-                });
-              })
-        ],
-      ),);
+    return Container(
+        padding: EdgeInsets.only(left: 16, right: 16),
+        decoration: BoxDecoration(
+            border: Border.all(color: lightGrayColor),
+            borderRadius: BorderRadius.circular(6)
+        ),
+        child: Theme(
+          data: Theme.of(context).copyWith(
+            highlightColor: Colors.transparent,
+            splashColor: Colors.transparent,
+          ),
+          child: DropdownButton(
+            value: _selectedFilter,
+            elevation: 0,
+            items: _filters.map((select)=>DropdownMenuItem(
+                child: Container(
+                    child: Text(select, style: Theme.of(context).textTheme.labelMedium)),
+                value: select)).toList(),
+            onChanged: (value){
+              setState(() {
+                _selectedFilter = value!;
+              });},
+            underline: Container(),
+            icon: Icon(Icons.arrow_drop_down),
+            style: Theme.of(context).textTheme.labelMedium!.copyWith(color: blackColor),
+            dropdownColor: Colors.white,
+            alignment: Alignment.center,
+          ),
+        )
+    );
   }
 }
